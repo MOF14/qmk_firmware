857a281ee4622a87dfa4692a1c9b210619955e4b
MOF14 <michael.oliver.forward@gmail.com>
2022-01-04 00:02:15 -0500

    Minor changes

--- keyboards/crkbd/keymaps/MOF14/keymap.c
+++ keyboards/crkbd/keymaps/MOF14/keymap.c
@@ -140,15 +140,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
   ),
 
-  [_CAD_FN] = LAYOUT_split_3x6_3(
+  [_CAD_FN] = LAYOUT_split_3x6_3( //CAD layout for Fusion360
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX, KC_ESC,      KC_Q,   KC_W,   KC_E,   KC_R,                     XXXXXXX,      KC_P7,   KC_P8,   KC_P9,  KC_0,  KC_BSPC,
+      KC_ESC,   KC_ENT,   KC_Q,   KC_W,   KC_E,   KC_R,                           XXXXXXX, KC_P7,   KC_P8,   KC_P9,  KC_0,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, KC_F,      KC_A,   KC_S,   KC_D,   KC_F,                         KC_NLCK,  KC_P4,   KC_P5,   KC_P6,  KC_PMNS, KC_PAST,
+      KC_LCTL,    KC_F,   KC_A,   KC_S,   KC_D,   KC_L,                         KC_NLCK,  KC_P4,   KC_P5,   KC_P6,  KC_PMNS, KC_PAST,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, KC_Z,      KC_Z,   KC_X,   KC_C,   KC_C,                         KC_P0,  KC_P1,   KC_P2,   KC_P3,  KC_PPLS, KC_PENT,
+      KC_LSFT,    KC_Z,   KC_Z,   KC_X,   KC_C,   KC_C,                         KC_P0,  KC_P1,   KC_P2,   KC_P3,  KC_PPLS, KC_PENT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_ENT,  KC_N,  TG(_CAD_FN),    MO(_ADJUST), KC_P0, KC_PDOT
+                                        KC_N, LCTL_T(KC_ENT), TG(_CAD_NUM),    MO(_ADJUST), KC_P0, KC_PDOT
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -161,7 +161,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
 LT(_ADJUST, KC_NUM), XXXXXXX,  KC_1,    KC_P2,    KC_3,    KC_0,                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_BSPC, KC_ENT,TG(_CAD_FN),    MO(_ADJUST),  KC_TRNS,  KC_TRNS
+                                          KC_BSPC, KC_ENT,TG(_CAD_NUM),    MO(_ADJUST),  KC_TRNS,  KC_TRNS
                                       //`--------------------------'  `--------------------------'
   ),
 
